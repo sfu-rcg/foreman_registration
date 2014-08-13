33cd91d31bdc3effd72577204b66ef9f8474323b
Brian Warsing <bcw@sfu.ca>
2014-08-13 11:03:35 -0700

    fix engine config and remove scar tissue

--- lib/foreman_registration/engine.rb
+++ lib/foreman_registration/engine.rb
@@ -1,22 +1,12 @@
 module ForemanRegistration
   class Engine < ::Rails::Engine
 
-    # config.to_prepare do
-    #   if SETTINGS[:version].to_s.to_f >= 1.2
-    #     # Foreman 1.2
-    #     Host::Managed.send :include, Host::HostExtensions
-    #   else
-    #     # Foreman < 1.2
-    #     Host.send :include, Host::HostExtensions
-    #   end
-    # end
-
     initializer 'foreman_registration.register_plugin', :after=> :finisher_hook do |app|
       Foreman::Plugin.register :foreman_registration do
         requires_foreman '>= 1.5'
 
         security_block :foreman_registration do
-          permission :register_node, { 'api/v2/register' => [:register] }
+          permission :register_node, { 'api/v2/registrations' => [:register] }
         end
 
         role "Registrar", [:register_node]
