e0d3dcc40678010e0312ac5d3435756826e72daf
Brian Warsing <bcw@sfu.ca>
2014-11-13 12:37:30 -0800

    update method list and ensure our settings model is loaded early

--- lib/foreman_registration/engine.rb
+++ lib/foreman_registration/engine.rb
@@ -1,6 +1,11 @@
 module ForemanRegistration
   class Engine < ::Rails::Engine
 
+    initializer 'foreman_registration.load_default_settings', :before => :load_config_initializers do |app|
+      file = '../../../app/models/settings/foreman_registration.rb'
+      require_dependency File.expand_path(file, __FILE__) if (Setting.table_exists? rescue(false))
+    end
+
     initializer 'foreman_registration.register_plugin', :after=> :finisher_hook do |app|
       Foreman::Plugin.register :foreman_registration do
         requires_foreman '>= 1.5'
@@ -9,11 +14,11 @@ module ForemanRegistration
           permission :register_node, {
             'api/v2/registrations' => [
               :register,
+              :reset,
               :decommission,
-              :environment_list,
-              :environment_id_by_name,
-              :hostgroup_list,
-              :hostgroup_id_by_name,
+              :registration_status,
+              :reg_environments,
+              :reg_hostgroups,
             ]
           }
         end
